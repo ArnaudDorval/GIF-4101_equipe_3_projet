01c6d6f9de848fd93759b27000063ab3ff42111c
Arnaud Dorval <38009779+ArnaudDorval@users.noreply.github.com>
2021-10-17 15:18:52 -0400

    test python push

--- Data_Scraper/experimentation/git_add_test.py
+++ Data_Scraper/experimentation/git_add_test.py
@@ -1,15 +1,26 @@
 from git import Repo
+from datetime import date
+import os
+import logging
 
-
-PATH_OF_GIT_REPO = r'C:\Users\arnau\OneDrive\Documents\Universite\Automne 2021\AI\projet\GIF-4101_equipe_3_projet.git'  # make sure .git folder is properly configured
+#PATH_OF_GIT_REPO = r'C:/Users/arnau/OneDrive/Documents/Universite/Automne 2021/AI/projet/GIF-4101_equipe_3_projet.git'  # make sure .git folder is properly configured
+PATH_OF_GIT_REPO = r'C:\Users\arnau\OneDrive\Documents\Universite\Automne 2021\AI\projet\GIF-4101_equipe_3_projet\.git'
 COMMIT_MESSAGE = 'test python push'
 
+
 def git_push():
-    try:
-        repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add(update=True)
-        repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='origin')
-        origin.push()
-    except:
-        print('Some error occured while pushing the code')
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add(update=True)
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='origin')
+    origin.push()
+    #        print('Some error occured while pushing the code')
+
+
+file_name = 'data/' + str(date.today()) + "-test.txt"
+
+f = open("test_txt.txt", 'w')
+f.write("Allo ce ceci est un test")
+f.close()
+
+git_push()
