9175a4045c35cff587b50013fab93144fe85ba5f
Arnaud Dorval <arnaud.dorval-leblanc.1@ulaval.ca>
2021-10-18 09:32:31 -0400

    new script only hospital

--- Data_Scraper/read_csv.py
+++ Data_Scraper/read_csv.py
@@ -7,7 +7,7 @@ file_name = 'data/' + str(date.today()) + "-data"
 
 #ouverture du fichier pour traitement
 #data = pd.read_csv(file_name + ".csv", sep=";")
-data = pd.read_csv("data/2021-10-15-data.csv", sep=";")
+data = pd.read_csv("data/2021-10-14-data.csv", sep=";")
 data = data[["Nom_etablissement", "Nom_installation", "No_permis_installation", "Nombre_de_civieres_fonctionnelles",
                "Nombre_de_civieres_occupees", "Nombre_de_patients_sur_civiere_plus_de_24_heures",
                "Nombre_de_patients_sur_civiere_plus_de_48_heures", "Heure_de_l'extraction_(image)", "Mise_a_jour",
