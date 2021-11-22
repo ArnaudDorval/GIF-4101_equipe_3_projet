4ac25a32e1dcfb8f41d91545e33c87eafa941d99
Ariane Boivin <ariane.boivin.4@ulaval.ca>
2021-11-22 10:05:19 -0500

    meilleur_modele ajustee

--- Regression/fonctions_reg.py
+++ Regression/fonctions_reg.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import KFold
 from sklearn import datasets
 from matplotlib import pyplot
 from math import sqrt, ceil
-from load_dataset import Data
+from data import Data
 
 
 
@@ -24,6 +24,7 @@ def appliquer_regression(data, target, k, fit_intercept=True, shuffle=True):
 
     return (sum(liste_erreurs)/len(liste_erreurs))
 
+# Retourne une liste contenant les index des variables utilisees par le meilleur modele et l'erreur de ce modele
 def meilleur_modele(data, target):
     nb_vars = data.shape[1]
 
@@ -32,16 +33,19 @@ def meilleur_modele(data, target):
         for idx_2 in range(idx_1 + 1, nb_vars + 1):
             jd = data[:,idx_1:idx_2]
             err = appliquer_regression(jd, target, k=3)
-            resultats.append([idx_1, idx_2, err])
+            print(err)
+            vars = [i for i in range(idx_1, idx_2+1)]
+            resultats.append([vars, err])
 #    return resultats
     resultats = np.array(resultats)
-    meill_err_idx = np.argmax(resultats[:,2])
+    meill_err_idx = np.argmin(resultats[:,1])
     return resultats[meill_err_idx,:]
 
 
 def main():
     x,y = Data().get_x_y()
     appliquer_regression(x.values, y.values, 2)
+    print(meilleur_modele(x.values, y.values))
     #diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
     #nb_vars = diabetes_X.shape[1]
     #carre = int(sqrt(nb_vars))
